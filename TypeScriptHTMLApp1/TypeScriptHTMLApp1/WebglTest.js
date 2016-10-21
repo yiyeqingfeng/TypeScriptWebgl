a413489c355aeaccbd25d229b6412c56c01c3324
liumeng <liumeng@cafegame.cn>
2016-10-21 11:05:30 +0800

    xiu

--- TypeScriptHTMLApp1/TypeScriptHTMLApp1/WebglTest.js
+++ TypeScriptHTMLApp1/TypeScriptHTMLApp1/WebglTest.js
@@ -1,6 +1,36 @@
 var WebglTest = (function () {
     function WebglTest() {
+        this.testVertexCode = '\
+    attribute vec3 aVertexPosition;\
+    uniform mat4 uPMatrix;\
+    void main(void){\
+        gl_Position = uPMatrix*uMVMatrix*vec4(aVertexPosition,1.0);\
+    }';
+        this.testFragmentCode = '\
+    void main(void){\
+    gl_FragColor=vec4(1.0,1.0,1.0,1.0);\
+    }';
+        this.canvas = document.getElementById("renderCanvas");
+        this.webgl = this.canvas.getContext('experimental-webgl') || this.canvas.getContext('webgl');
     }
+    WebglTest.prototype.init = function () {
+    };
+    WebglTest.prototype.getVertShader = function () {
+        if (this.vertShader)
+            return this.vertShader;
+        var vertShader = this.webgl.createShader(this.webgl.VERTEX_SHADER);
+        this.webgl.shaderSource(vertShader, this.testVertexCode);
+        this.webgl.compileShader(vertShader);
+        return vertShader;
+    };
+    WebglTest.prototype.getFragShader = function () {
+        if (this.fragShader)
+            return this.fragShader;
+        var fragShader = this.webgl.createShader(this.webgl.FRAGMENT_SHADER);
+        this.webgl.shaderSource(fragShader, this.testFragmentCode);
+        this.webgl.compileShader(fragShader);
+        return fragShader;
+    };
     return WebglTest;
 }());
 //# sourceMappingURL=WebglTest.js.map
--- TypeScriptHTMLApp1/TypeScriptHTMLApp1/WebglTest.ts
+++ TypeScriptHTMLApp1/TypeScriptHTMLApp1/WebglTest.ts
@@ -1,7 +1,49 @@
 ﻿class WebglTest
 {
     constructor()
-    { }
+    {
+        this.canvas = <HTMLCanvasElement>document.getElementById("renderCanvas");
+        this.webgl = this.canvas.getContext('experimental-webgl') || this.canvas.getContext('webgl');
+    }
 
+    private canvas: HTMLCanvasElement;
+    private webgl: WebGLRenderingContext;
+    private testVertexCode = '\
+    attribute vec3 aVertexPosition;\
+    uniform mat4 uPMatrix;\
+    void main(void){\
+        gl_Position = uPMatrix*uMVMatrix*vec4(aVertexPosition,1.0);\
+    }';
 
+    private testFragmentCode = '\
+    void main(void){\
+    gl_FragColor=vec4(1.0,1.0,1.0,1.0);\
+    }';
+
+    public init()
+    {
+        
+
+    }
+
+    private vertShader: WebGLShader;
+    private fragShader: WebGLShader;
+
+    private getVertShader(): WebGLShader
+    {
+        if (this.vertShader) return this.vertShader;
+        var vertShader = this.webgl.createShader(this.webgl.VERTEX_SHADER);
+        this.webgl.shaderSource(vertShader, this.testVertexCode);
+        this.webgl.compileShader(vertShader);
+        return vertShader;
+    }
+
+    private getFragShader(): WebGLShader
+    {
+        if (this.fragShader) return this.fragShader;
+        var fragShader = this.webgl.createShader(this.webgl.FRAGMENT_SHADER);
+        this.webgl.shaderSource(fragShader, this.testFragmentCode);
+        this.webgl.compileShader(fragShader);
+        return fragShader;
+    }
 }
